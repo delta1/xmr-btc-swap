44eb4174cd57de630729bc492cfb4b087c07ed1d
Byron Hambly <bhambly@blockstream.com>
2022-11-08 19:55:36 +0200

    debugging

--- swap/tests/bdk.sh
+++ swap/tests/bdk.sh
@@ -4,6 +4,10 @@ set -euxo pipefail
 
 VERSION=0.11.1
 
+mkdir bdk
+cp ./target/debug/swap bdk/swap-current
+pushd bdk
+
 echo "download swap $VERSION"
 curl -L "https://github.com/comit-network/xmr-btc-swap/releases/download/${VERSION}/swap_${VERSION}_Linux_x86_64.tar" | tar xv
 ls -alht
@@ -14,12 +18,12 @@ echo "create mainnet wallet with $VERSION"
 ./swap --version || exit 1
 ./swap --data-base-dir . --debug balance || exit 1
 echo "check mainnet wallet with this version"
-./target/debug/swap --version || exit 1
-./target/debug/swap --data-base-dir . --debug balance || exit 1
+./swap-current --version || exit 1
+./swap-current --data-base-dir . --debug balance || exit 1
 
 echo "create testnet wallet with $VERSION"
 ./swap --testnet --data-base-dir . --debug balance || exit 1
 echo "check testnet wallet with this version"
-./target/debug/swap --testnet --data-base-dir . --debug balance || exit 1
+./swap-current --testnet --data-base-dir . --debug balance || exit 1
 
 exit 0
