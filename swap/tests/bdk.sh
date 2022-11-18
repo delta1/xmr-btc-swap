99f4382889d654ee7317ebe1c254bc9c2073c121
Byron Hambly <bhambly@blockstream.com>
2022-11-18 15:15:39 +0200

    derp2

--- swap/tests/bdk.sh
+++ swap/tests/bdk.sh
@@ -11,19 +11,19 @@ pushd bdk
 
 echo "download swap $VERSION"
 #
-# curl -L "https://github.com/comit-network/xmr-btc-swap/releases/download/${VERSION}/swap_${VERSION}_Linux_x86_64.tar" | tar xv
-cp /tmp/swap0.11.x ./swap
+curl -L "https://github.com/comit-network/xmr-btc-swap/releases/download/${VERSION}/swap_${VERSION}_Linux_x86_64.tar" | tar xv
+# cp /tmp/swap0.11.x ./swap
 
 echo "create testnet wallet with $VERSION"
 ./swap --testnet --data-base-dir . --debug balance || exit 1
 echo "check testnet wallet with this version"
 ./swap-current --testnet --data-base-dir . --debug balance || exit 1
 
-# echo "create mainnet wallet with $VERSION"
-# ./swap --version || exit 1
-# ./swap --data-base-dir . --debug balance || exit 1
-# echo "check mainnet wallet with this version"
-# ./swap-current --version || exit 1
-# ./swap-current --data-base-dir . --debug balance || exit 1
+echo "create mainnet wallet with $VERSION"
+./swap --version || exit 1
+./swap --data-base-dir . --debug balance || exit 1
+echo "check mainnet wallet with this version"
+./swap-current --version || exit 1
+./swap-current --data-base-dir . --debug balance || exit 1
 
 exit 0
